033754587a34521e2a9210ea50cd650d3846d58c
nikithadugya <nikithadugya@gmail.com>
2025-11-19 22:32:34 +0530

    Some message

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Please enter your PIN number"
+read -s PIN_NUMBER # PIN_NUMBER is variable and to get input from user we use read command. -s means no ne can see the password wile we are typing
+
+echo "PIN entered is : $PIN_NUMBER"
