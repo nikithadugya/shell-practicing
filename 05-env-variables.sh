fae3779aaae0e300cdf298fd401739a0f00ab9cc
nikithadugya <nikithadugya@gmail.com>
2025-11-20 19:24:25 +0530

    Some message

--- 05-env-variables.sh
+++ 05-env-variables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # here we are not declaring the variable but just calling it so in Mobxterm session we will declare the vraiable by export $COURSE = "something" 
-#but this will go after session expiry so we declare in the ./bashrc file add export course = " " so that evrytime the session starts it runs the ./bashrc file so env variable also will exist.
+#but this will go after session expiry so we declare in the ./bashrc file (come out of the repository location) add export course = " " so that evrytime the session starts it runs the ./bashrc file so env variable also will exist.
 echo "Course name  is : $COURSE"
