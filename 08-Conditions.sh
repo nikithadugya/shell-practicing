adb1214a23d91272f65e3aba5bb66a99ca406328
nikithadugya <nikithadugya@gmail.com>
2025-11-20 22:19:30 +0530

    Some message

---
+++ 08-Conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -lt 10]; then
+    echo "Given number $NUMBER is less than 10"
+elif [ $NUMBER -eq 10]; then
+    echo "Given number $NUMBER is equal 10"
+else
+    echo "Given number $NUMBER greater than 10"
+fi
+
