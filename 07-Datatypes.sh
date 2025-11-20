e5cfe4ba8e24736925df533a37f796b21ef17cf2
nikithadugya <nikithadugya@gmail.com>
2025-11-20 22:03:28 +0530

    Some message

--- 07-Datatypes.sh
+++ 07-Datatypes.sh
@@ -7,4 +7,9 @@ NAME=DevOps
 
 SUM=$(($NUMBER1+$NUMBER2+$NAME))  # 300
 
-echo "SUM is:$SUM"
+echo "SUM is:{$SUM}"
+LEADERS=("MODI","PUTHIN","TRUDO","TRUMP")
+
+echo "All leaders: ${LEADERS[@]}" # prints all leaders
+echo "First leader: ${LEADERS[0]}" # prints MODI
+echo "Nth leader: ${LEADERS[10]}" #prints nothing
