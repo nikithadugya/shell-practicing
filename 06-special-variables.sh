e16a035aba3438d5cc77b9d60e0a242bca5ea464
nikithadugya <nikithadugya@gmail.com>
2025-11-20 19:36:41 +0530

    Some message

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -4,3 +4,8 @@ echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "Script name: $0"
 echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory pf user: $HOME"
+echo "PID of this current script:$$"
+sleep 50 & # this is running in the background
+echo "PID of the last command in background is: $!"
