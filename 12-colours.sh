9ccc41cedf3ab6f203bda413343b5ba3ccc07a4a
nikithadugya <nikithadugya@gmail.com>
2025-11-21 19:42:11 +0530

    Some message

--- 12-colours.sh
+++ 12-colours.sh
@@ -7,7 +7,8 @@ Normal="\e[0m"
 
 echo -e "$G Hello world $Normal"  #Here -e indicates enabling and $G indicates Green colour and we placed $Normal at end because $G continues to next line also here Normal means white colour
 echo "Check this colour"
-echo "OK"
+echo "OKay"
 echo "yaaa"
 echo "Noooo"
 echo "we will see"
+echo "haaa"
