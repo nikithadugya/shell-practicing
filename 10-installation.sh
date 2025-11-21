0d4686740802b26ade7b2b85a6d6755f254f463e
nikithadugya <nikithadugya@gmail.com>
2025-11-21 12:27:36 +0530

    Some message

--- 10-installation.sh
+++ 10-installation.sh
@@ -3,10 +3,17 @@
 USERID=$(id -u)
 
 if($USERID -eq 0); then
-    dnf install mysql -y
-   # echo "ERROR:: Please run this script with root(sudo) privilage"
-    #exit 1 #any number is failure other than 0
+    echo "ERROR:: Please run this script with root(sudo) privilage"
+    exit 1 #any number is failure other than 0
 else
     echo "ERROR:: Please run this script with root(sudo) privilage"
+fi
+
+dnf install mysql -y
+
+if ($? -ne 0)
+    echo "ERROR:: Installation mysql is failure""
+else
+    echo "Installation is success"
 fi
 
